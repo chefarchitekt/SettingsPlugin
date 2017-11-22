89947cfb53fd2c865d4ccdc4b7f086f27ef38d75
Prashant Cholachagudda <prchol@microsoft.com>
2017-11-22 16:23:51 +0530

    pass path to the shell script

--- tests/runtests.sh
+++ tests/runtests.sh
@@ -3,7 +3,7 @@
 echo $OutputPath
 
 mono --debug Touch.Server.exe \
---launchsim $OutputPath/RefractoredXamSettingsNUnitTestiOSUnified.app \
+--launchsim $0/RefractoredXamSettingsNUnitTestiOSUnified.app \
 -autoexit -skipheader  \
--logfile=$OutputPath/test_results.xml \
+-logfile=$0/test_results.xml \
 --device=":v2:runtime=com.apple.CoreSimulator.SimRuntime.iOS-11-1,devicetype=com.apple.CoreSimulator.SimDeviceType.iPhone-SE"
