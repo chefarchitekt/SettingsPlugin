72527d289ce76e203f725d19bd4abe88645b3817
Prashant Cholachagudda <prchol@microsoft.com>
2017-11-23 15:02:15 +0530

    Fix the app path

--- tests/runtests.sh
+++ tests/runtests.sh
@@ -3,7 +3,7 @@
 echo 'Output path = ' $1
 
 mono --debug Touch.Server.exe \
---launchsim Plugin.Settings.NUnitTest.Android/bin/Debug/RefractoredXamSettingsNUnitTestiOSUnified.app \
+--launchsim Plugin.Settings.NUnitTest.iOS/bin/iPhoneSimulator/Debug/RefractoredXamSettingsNUnitTestiOSUnified.app \
 -autoexit -skipheader  \
 -logfile=$1/test_results.xml \
 --device=":v2:runtime=com.apple.CoreSimulator.SimRuntime.iOS-11-1,devicetype=com.apple.CoreSimulator.SimDeviceType.iPhone-SE"
